3053a8d599b4f0544150f5ad2f4346d88fe245b1
chaojun_Lu <chaojun_Lu@clevo.com.tw>
2024-08-02 10:27:47 +0800

    Add more action to prevent error issue

--- CopyRoomPy/UIAutoControl.py
+++ CopyRoomPy/UIAutoControl.py
@@ -23,23 +23,24 @@ class UIAutoControl:
             auto.ShowDesktop()
             window = auto.WindowControl(searchDepth=1, Name="ALL-300G")
             if not window.Exists():
-                # subprocess.Popen('D:\\WorkDocument\\CopyRoomSW\Hi-Lo\\ALL-300G\\ALL-300G.exe', shell=True) #office computer
-                subprocess.Popen('C:\\Program Files (x86)\\Hi-Lo\\ALL-300G\\ALL-300G.exe', shell=True) #SMT computer
+                subprocess.Popen('D:\\WorkDocument\\CopyRoomSW\Hi-Lo\\ALL-300G\\ALL-300G.exe', shell=True) #office computer
+                # subprocess.Popen('C:\\Program Files (x86)\\Hi-Lo\\ALL-300G\\ALL-300G.exe', shell=True) #SMT computer
             window.SetActive()
-            # window.MenuItemControl(searchDepth=3, Name='File').Click()  #office computer
-            window.MenuItemControl(searchDepth=3, Name='文件').Click()  #SMT computer
+            window.MenuItemControl(searchDepth=3, Name='File').Click()  #office computer
+            # window.MenuItemControl(searchDepth=3, Name='文件').Click()  #SMT computer
             # 移动鼠标到指定坐标（x=100, y=100）
             mouse.move(coords=(500, 500))
-            # window.MenuItemControl(searchDepth=3, Name="Open Job File").Click() #office computer
-            window.MenuItemControl(searchDepth=3, Name="打开工程文件").Click() #SMT computer
+            window.MenuItemControl(searchDepth=3, Name="Open Job File").Click() #office computer
+            # window.MenuItemControl(searchDepth=3, Name="打开工程文件").Click() #SMT computer
             print("File_name=" + file_name)
             jobFile = auto.WindowControl(searchDepth=2, Name="Open Job File")
-            # jobFile.EditControl(searchDepth=3, Name='檔案名稱(N):').SendKeys(file_name) #office computer
-            jobFile.EditControl(searchDepth=3, Name='文件名(N):').SendKeys(file_name) #SMT computer
-            # jobFile.ButtonControl(searchDepth=3, Name='開啟(O)').Click() #office computer
-            jobFile.ButtonControl(searchDepth=3, Name='打开(O)').Click() #SMT computer
+            jobFile.EditControl(searchDepth=3, Name='檔案名稱(N):').SendKeys(file_name) #office computer
+            # jobFile.EditControl(searchDepth=3, Name='文件名(N):').SendKeys(file_name) #SMT computer
+            jobFile.ButtonControl(searchDepth=3, Name='開啟(O)').Click() #office computer
+            # jobFile.ButtonControl(searchDepth=3, Name='打开(O)').Click() #SMT computer
             time.sleep(10)
-            auto_window = auto.WindowControl(searchDepth=2, Name="Auto")
+            auto_window = auto.WindowControl(searchDepth=2, Name="Auto") #office computer
+            # auto_window = auto.WindowControl(searchDepth=2, Name="自动") #SMT computer
             if auto_window.Exists():
                 self.controlBurn(data_dict)
 
@@ -54,7 +55,9 @@ class UIAutoControl:
         try:
             window = auto.WindowControl(searchDepth=1, Name=u" 群沃自动烧录系统")
             if not window.Exists():
-                subprocess.Popen('D:\\WorkDocument\\CopyRoomSW\\Burn_V1.0.1.12 -hl\\Burn\\bin\\Debug\\Burn.exe', shell=True)
+                subprocess.Popen('D:\\WorkDocument\\CopyRoomSW\\Burn_V1.0.1.12 -hl\\Burn\\bin\\Debug\\Burn.exe', shell=True) #office computer
+                # subprocess.Popen('D:\\Burn_V1.0.1.12 -hl\\Burn\\bin\\Debug\\Burn.exe', shell=True) #SMT Computer
+            time.sleep(10)
             window.SetActive()
             homeBtn = window.ButtonControl(searchDepth=6, AutomationId="but_Homing")
             if homeBtn.Exists():
--- CopyRoomPy/printtest.py
+++ CopyRoomPy/printtest.py
@@ -1,10 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 
-
-root = tk.Tk()
-root.withdraw()  # 隐藏主窗口
-messagebox.showwarning("警告", "这是一个警告框！")
-print("1234567890")
-root.destroy()
-root.mainloop()
+rootDialog = tk.Tk()
+rootDialog.withdraw()  # 隐藏主窗口
+messagebox.showinfo("提示", "IC Burn自動化已經結束, 請手動完成下面其他步驟")
+rootDialog.destroy()
+rootDialog.mainloop()
+print(456)
